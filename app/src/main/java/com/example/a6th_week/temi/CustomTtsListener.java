dac4ebe5e20c78ce5ecd40bb9aedad2a97b7e8a8
ssongmina <thdalsdk5498@naver.com>
2026-05-17 23:26:23 +0900

    initial setting

---
+++ app/src/main/java/com/example/a6th_week/temi/CustomTtsListener.java
@@ -0,0 +1,17 @@
+package com.example.a6th_week.temi;
+
+
+import android.util.Log;
+
+import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.TtsRequest;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CustomTtsListener implements Robot.TtsListener{
+    @Override
+    public void onTtsStatusChanged(@NotNull TtsRequest ttsRequest) {
+        Log.i("Temi","Tts Status changed! "+ttsRequest.getStatus()+" - Custom");
+    }
+}
+
